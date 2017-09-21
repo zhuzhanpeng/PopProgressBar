a0359fbae2fa86ff611d1f9088ad979f2b6fff4e
edison <1609309613@qq.com>
2017-09-21 22:52:21 +0800

    PopWindow/ProgressBar

---
+++ app/src/main/java/onairm/com/popprogressbar/MainActivity.java
@@ -0,0 +1,16 @@
+package onairm.com.popprogressbar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_main);
+    }
+
+
+}
